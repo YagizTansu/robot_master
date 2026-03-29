95c181ee0cd041d6e00b08f2c18c63346f117922
Yagiz Tansu <yagiz.tansu99@gmail.com>
2026-03-29 17:21:18 +0200

    feat: Add cmd_vel relay node and update launch/config files for tricycle controller

--- boa_new_urdf_description/setup.py
+++ boa_new_urdf_description/setup.py
@@ -26,6 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'cmd_vel_relay = boa_new_urdf_description.cmd_vel_relay:main',
         ],
     },
 )
