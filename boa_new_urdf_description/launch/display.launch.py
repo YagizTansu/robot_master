9692663be2990490abab444a0a8d9f8f7336d4d3
Yagiz Tansu <yagiz.tansu99@gmail.com>
2026-03-29 16:10:34 +0200

    feat: Update URDF and launch files for improved robot model configuration

--- boa_new_urdf_description/launch/display.launch.py
+++ boa_new_urdf_description/launch/display.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
+        name='boa_state_publisher',
         parameters=[
             {'robot_description': robot_urdf}
         ]
